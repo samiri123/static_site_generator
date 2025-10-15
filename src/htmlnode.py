bc33db18c2e7d393b318deaba9ffa33e5739e64a
samiri123 <133253978+samiri123@users.noreply.github.com>
2025-10-15 21:10:59 +0200

    parent node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,19 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no value")
+        if self.children is None:
+            raise ValueError("ParentNode needs children")
+        children_result = ""
+        for child in self.children:
+            children_result += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_result}</{self.tag}>"
+
