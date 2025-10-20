c5f839d07a0a048a6dfab244ea952f319b13ce7b
samiri123 <133253978+samiri123@users.noreply.github.com>
2025-10-20 21:06:08 +0200

    node splitter for links and images

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+from extract_markdown import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type: TextType):
     new_nodes = []
@@ -20,3 +21,62 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type: TextType):
                         split_nodes.append(TextNode(text, TextType.TEXT))
                 new_nodes.extend(split_nodes)
     return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        count = 0
+        sections = []
+        while count < len(images):
+            sections = original_text.split(f"![{images[count][0]}]({images[count][1]})", 1)
+            if sections[0] != "":
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+            split_nodes.append(TextNode(images[count][0], TextType.IMAGE, images[count][1]))
+            original_text = sections[1]
+            count += 1
+        new_nodes.extend(split_nodes)
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        count = 0
+        sections = []
+        while count < len(links):
+            sections = original_text.split(f"[{links[count][0]}]({links[count][1]})", 1)
+            if sections[0] != "":
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+            split_nodes.append(TextNode(links[count][0], TextType.LINK, links[count][1]))
+            original_text = sections[1]
+            count += 1
+        new_nodes.extend(split_nodes)
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+node = TextNode(
+    "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+    TextType.TEXT,
+)
+new_nodes = split_nodes_link([node])
+print(new_nodes)
