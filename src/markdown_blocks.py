fad9b096f39a5942ee986eb69e3446ee64cdc41d
samiri123 <133253978+samiri123@users.noreply.github.com>
2025-10-24 20:10:44 +0200

    html markdown block types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 
 def markdown_to_blocks(markdown: str):
     blocks = []
@@ -8,3 +18,24 @@ def markdown_to_blocks(markdown: str):
             continue
         blocks.append(block)
     return blocks
+
+
+def block_to_block_type(block: str):
+    md_headings = ["#", "##", "###", "####", "#####", "######"]
+    if block.split()[0] in md_headings:
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    if all(line.startswith(">") for line in block.split("\n")):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in block.split("\n")):
+        return BlockType.UNORDERED_LIST
+    if block.startswith("1. "):
+        i = 1
+        for line in block.split("\n"):
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+    
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,8 +1,76 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestMarkdownToHTML(unittest.TestCase):
+    
+    def __init__(self, methodName = "runTest"):
+        super().__init__(methodName)
+        self.md = """
+This is another paragraph with _italic_ text and `code` here  
+This is the same paragraph on a new line  
+
+# This is an H1 heading  
+
+### This is an H3 heading  
+
+```python
+def example():
+    return "code block"
+```
+
+> This is a blockquote  
+> It can span multiple lines  
+
+- This is an unordered list  
+- with items  
+- and more items  
+
+1. This is an ordered list  
+2. with numbered items  
+3. in sequence
+"""     
+        self.blocks = markdown_to_blocks(self.md)
+        
+        
+    
+    def test_paragraph(self):
+        paragraph = self.blocks[0]
+        print(paragraph)
+        result = block_to_block_type(paragraph)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+    
+    def test_head_1(self):
+        heading_1 = self.blocks[1]
+        result = block_to_block_type(heading_1)
+        self.assertEqual(result, BlockType.HEADING)
+        
+    def test_head_3(self):
+        heading_3 = self.blocks[2]
+        result = block_to_block_type(heading_3)
+        self.assertEqual(result, BlockType.HEADING)
+    
+    def test_code(self):
+        code = self.blocks[3]
+        result = block_to_block_type(code)
+        self.assertEqual(result, BlockType.CODE)
+    
+    def test_quote(self):
+        quote = self.blocks[4]
+        result = block_to_block_type(quote)
+        self.assertEqual(result, BlockType.QUOTE)
+
+    def test_unordered_list(self):
+        unordered_list = self.blocks[5]
+        result = block_to_block_type(unordered_list)
+        self.assertEqual(result, BlockType.UNORDERED_LIST)
+    
+    def test_ordered_list(self):
+        ordered_list = self.blocks[6]
+        result = block_to_block_type(ordered_list)
+        self.assertEqual(result, BlockType.ORDERED_LIST)
+        
+        
     def test_markdown_to_blocks(self):
         md = """
 This is **bolded** paragraph
