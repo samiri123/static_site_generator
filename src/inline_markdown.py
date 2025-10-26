c8a4063fb2db970110cbd4a8b4a32693ab5ed2be
samiri123 <133253978+samiri123@users.noreply.github.com>
2025-10-26 18:06:48 +0100

    markdown to html

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -8,17 +8,17 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type: TextType):
             new_nodes.append(old_node)
         else:
             split_nodes = []
-            splitted_node_text = old_node.text.split(delimiter)
-            if len(splitted_node_text) % 2 == 0:
+            sections = old_node.text.split(delimiter)
+            if len(sections) % 2 == 0:
                 raise Exception("Opening delimiter must be closed with same delimiter.")
             else:
-                for text in splitted_node_text:
-                    if text == "":
+                for i in range(len(sections)):
+                    if sections[i] == "":
                         continue
-                    if text == text.strip():
-                        split_nodes.append(TextNode(text, text_type))
+                    if i % 2 == 0:
+                        split_nodes.append(TextNode(sections[i], TextType.TEXT))
                     else:
-                        split_nodes.append(TextNode(text, TextType.TEXT))
+                        split_nodes.append(TextNode(sections[i], text_type))
                 new_nodes.extend(split_nodes)
     return new_nodes
 
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from htmlnode import HTMLNode, ParentNode, LeafNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node, TextNode, TextType
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -38,4 +41,88 @@ def block_to_block_type(block: str):
             i += 1
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
-    
+
+def markdown_to_html_node(markdown):
+    children = []
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_html = block_to_html_node(block)
+        children.append(block_html)
+    return ParentNode("div", children)
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    match block_type:
+        case BlockType.PARAGRAPH:
+            return paragraph_to_html_node(block)
+        case BlockType.HEADING:
+            return heading_to_html_node(block)
+        case BlockType.CODE: 
+            return code_to_html_node(block)
+        case BlockType.QUOTE:
+            return quote_to_html_node(block)
+        case BlockType.UNORDERED_LIST:
+            return unordered_list_to_html_node(block)
+        case BlockType.ORDERED_LIST:
+            return ordered_list_to_html_node(block)
+        case _:
+            raise ValueError("invalid block type")
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    inline_html_nodes = []
+    for text_node in text_nodes:
+        inline_html_nodes.append(text_node_to_html_node(text_node))
+    return inline_html_nodes
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    row_text = " ".join(lines)
+    return ParentNode("p", text_to_children(row_text))
+
+def heading_to_html_node(block):
+    count_dashes = block.split()[0].count("#")
+    if count_dashes + 1 >= len(block):
+        raise ValueError("Heading must contain text")
+    elif 1 > count_dashes > 6:
+        raise ValueError("Heading must be between 1 and 6")
+    row_text = block[count_dashes+1:]
+    return ParentNode(f"h{count_dashes}", text_to_children(row_text))
+
+def code_to_html_node(block):
+    if not block.startswith("```") and block.endswith("```"):
+        raise ValueError("Not a code block")
+    row_text = block[4:-3]
+    lines = row_text.split("\n")
+    row_text = "\n".join(lines)
+    text_node = TextNode(row_text, TextType.CODE)
+    html_node = text_node_to_html_node(text_node)
+    return ParentNode("pre", [html_node])
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("not a valid quote")
+        new_lines.append(line.lstrip(">").strip())
+    row_text = " ".join(new_lines)
+    return ParentNode("blockquote", text_to_children(row_text))
+
+def ordered_list_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
+
+def unordered_list_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
