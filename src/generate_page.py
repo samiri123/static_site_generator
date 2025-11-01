636a603c506379ec5892685cbfa25d86cf08e277
samiri123 <133253978+samiri123@users.noreply.github.com>
2025-11-01 15:21:20 +0100

    genearte pages from a dir of markdown files to a dir of html files

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 from markdown_blocks import markdown_to_html_node, extract_title
 
 def generate_page(from_path, template_path, dest_path):
@@ -13,8 +14,6 @@ def generate_page(from_path, template_path, dest_path):
                     template_content = f.read()
             except Exception as e:
                 return f"Something went wrong during the reading of the files: {e}"
-            finally:
-                f.close()
         else:
             raise Exception(f"Path {template_path} does not exist.")
     else:
@@ -23,9 +22,9 @@ def generate_page(from_path, template_path, dest_path):
     html_string = markdown_to_html_node(from_path_content).to_html()
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", html_string)
+    project_abs = os.path.abspath(".")
+    dest_path_abs = os.path.abspath(dest_path)
     if not os.path.exists(dest_path):
-        project_abs = os.path.abspath(".")
-        dest_path_abs = os.path.abspath(dest_path)
         if not dest_path_abs.startswith(project_abs):
             raise Exception("Path of the src directory must be inside the project directory")
         try:
@@ -39,12 +38,33 @@ def generate_page(from_path, template_path, dest_path):
                 f.write(template_content)
         except Exception as e:
             return f"Error: something went wrong writing the file: {e}"
-        finally:
-            f.close()
+    else:
+        if os.path.isdir(dest_path_abs):
+            raise Exception("The src must be a file to write content")
+        try:
+            with open(dest_path_abs, 'w') as f:
+                f.write(template_content)
+        except Exception as e:
+            return f"Error: something went wrong writing the file: {e}"
     
 
-        
-        
-    
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    content_paths = get_paths(dir_path_content, [])
+    for path in content_paths:
+        relative_path = path.relative_to(dir_path_content)
+        dest_path = os.path.join(dest_dir_path, relative_path.parent)
+        os.makedirs(dest_path, exist_ok=True)
+        generate_page(str(path), template_path, os.path.join(dest_path, f"{path.stem}.html"))
+
+def get_paths(dir_path, path_list):
+    content_paths = os.listdir(dir_path)
+    for path in content_paths:
+        join_path = os.path.join(dir_path, path)
+        if os.path.isdir(join_path):
+            get_paths(join_path, path_list)
+        else:
+            if join_path.endswith("md"):
+                path_list.append(pathlib.Path(join_path))
+    return path_list           
     
     
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from copy_static import copy_static
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 def main():
     copy_static()
-    generate_page('content/index.md', './template.html', 'public/index.html')
+    generate_pages_recursive('content', './template.html', 'public')
     
 
 if __name__ == "__main__":
